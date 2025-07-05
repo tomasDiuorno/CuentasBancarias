9118c7f8c509e4293f098d605ea55908dbb4d226
Tomas Diuorno <tom.diuorno99@gmail.com>
2025-07-05 13:04:04 -0300

    agregado de archivos

---
+++ test/ar/edu/unlam/pb2/cuentasBancarias/CuentaTest.java
@@ -0,0 +1,18 @@
+package ar.edu.unlam.pb2.cuentasBancarias;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CuentaTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	}
+
+}
